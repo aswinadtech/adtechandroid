2553761bf1cb7af4062597b17af3d92d0c47a2b1
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2020-07-27 18:06:27 +0530

    Update AppiumFunctions.java

--- RegressionTest/src/test/java/twc/Regression/HandleWithAppium/AppiumFunctions.java
+++ RegressionTest/src/test/java/twc/Regression/HandleWithAppium/AppiumFunctions.java
@@ -561,12 +561,12 @@ public static void killADB() throws IOException, Exception{
 				capabilities.setCapability(capabilitydata[2][0], capabilitydata[2][Cap]); 
 				capabilities.setCapability(capabilitydata[3][0], capabilitydata[3][Cap]);
 				capabilities.setCapability(capabilitydata[7][0], capabilitydata[7][Cap]); 
-				capabilities.setCapability(capabilitydata[9][0], capabilitydata[9][Cap]);
+				//capabilities.setCapability(capabilitydata[9][0], capabilitydata[9][Cap]);
 				capabilities.setCapability(capabilitydata[10][0],capabilitydata[10][Cap]);
 				capabilities.setCapability(capabilitydata[12][0],capabilitydata[12][Cap]);
 			//	capabilities.setCapability("appActivity","com.weather.Weather.splash.SplashScreenActivity");
 				//capabilities.setCapability("appActivity","com.weather.android.daybreak.MainActivity");
-				//capabilities.setCapability("automationName","UiAutomator2");
+				capabilities.setCapability("automationName","UiAutomator2");
 				System.out.println("app : "+capabilitydata[10][Cap]);
 				logStep("app : "+capabilitydata[10][Cap]);
 				capabilities.setCapability(capabilitydata[13][0],capabilitydata[13][Cap]);
