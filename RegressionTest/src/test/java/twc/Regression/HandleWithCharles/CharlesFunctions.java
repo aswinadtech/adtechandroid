f7d68b41779d4cfcc81f198ba7559b9952c440d0
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2020-10-27 15:43:44 +0530

    Update CharlesFunctions.java

--- RegressionTest/src/test/java/twc/Regression/HandleWithCharles/CharlesFunctions.java
+++ RegressionTest/src/test/java/twc/Regression/HandleWithCharles/CharlesFunctions.java
@@ -80,8 +80,8 @@ public class CharlesFunctions extends Drivers{
 		FirefoxProfile profile = new FirefoxProfile();
 		profile.setPreference("network.proxy.type", 1);
 
-		//profile.setPreference("network.proxy.http", "192.168.1.15");
-		profile.setPreference("network.proxy.http", "192.168.43.163");
+		profile.setPreference("network.proxy.http", "192.168.1.15");
+		//profile.setPreference("network.proxy.http", "192.168.43.163");
 		//profile.setPreference("network.proxy.http", "10.30.168.211");
 		profile.setPreference("network.proxy.http_port", 8222);
 		profile.setPreference("browser.download.folderList", 2);
